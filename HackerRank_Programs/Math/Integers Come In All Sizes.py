b928b1bf6a877c24796fc90c536499784238bfdd
pu9052473 <pu9052473@gmail.com>
2024-06-14 22:22:33 +0530

    done 1 problems of Math

---
+++ HackerRank_Programs/Math/Integers Come In All Sizes.py
@@ -0,0 +1,11 @@
+# Input Format:
+# Integers a, b, c, and d are given on four separate lines, respectively.
+#
+# Output Format:
+# Print the result of (a**b + c**d) on one line.
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(a**b + c**d)
